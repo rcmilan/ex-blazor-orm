776625f3367e9482a32fa0945a97280eea62b135
Ricardo <13418519+rcmilan@users.noreply.github.com>
2023-05-04 09:50:32 -0300

    Inmemorydb (#1)
    * configs
    
    * post API

---
+++ BlazorORM.App/Data/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorORM.App.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+    }
+}
